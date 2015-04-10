335fd1528428f064c62f4b96ec0d1daa8df3ba80
Paul Marina <paul.marina@fortech.ro>
2015-04-10 09:57:27 +0300

    Fixed beans injection issue

--- src/ro/fortech/movies/DisplayMoviesDialog.java
+++ src/ro/fortech/movies/DisplayMoviesDialog.java
@@ -3,6 +3,7 @@ package ro.fortech.movies;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
@@ -15,18 +16,19 @@ import ro.fortech.movies.Movie;
 public class DisplayMoviesDialog implements Serializable {
 
 	private static final long serialVersionUID = -1718678896995420865L;
-	
-	
+
 	@Inject
 	private DisplayMoviesController displayCtrl;
 	private List<Movie> moviesList;
-	
-	public DisplayMoviesDialog(){
-		
+
+	public DisplayMoviesDialog() {
+	}
+
+	@PostConstruct
+	public void init() {
 		setMoviesList(displayCtrl.displayMovies());
 	}
-	
-	
+
 	public List<Movie> getMoviesList() {
 		return moviesList;
 	}
@@ -34,5 +36,4 @@ public class DisplayMoviesDialog implements Serializable {
 	public void setMoviesList(List<Movie> moviesList) {
 		this.moviesList = moviesList;
 	}
-	
 }
